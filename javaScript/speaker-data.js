b54a4518f7f994cf30f5f252726de6e5e335d939
imranhossainemi <blur-imran@hotmail.com>
2023-02-08 16:36:25 +0600

    Update speaker-data.js

--- javaScript/speaker-data.js
+++ javaScript/speaker-data.js
@@ -24,11 +24,39 @@ const speakerFour = {
   sWords: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt, quisquam!',
 };
 const speakerDataArr = [speakerOne, speakerTwo, speakerThree, speakerFour];
-const speaker = document.createElement('div');
-speaker.className = 'speaker';
-const s_img = document.createElement('div');
-s_img.className = 's-img';
-const s_details = document.createElement('div');
-s_details.className = 's-details';
-const s_bar = document.createElement('div');
-s_bar.className = 's-bar';
+const sDataArrLen = speakerDataArr.length;
+function speakerDataAdd() {
+  for (let b = 0; b < sDataArrLen; b++) {
+    const speaker = document.createElement('div');
+    speaker.className = 'speaker';
+    const s_img = document.createElement('div');
+    s_img.className = 's-img';
+    const s_details = document.createElement('div');
+    s_details.className = 's-details';
+    const s_bar = document.createElement('div');
+    s_bar.className = 's-bar';
+    const imgSpeaker = document.createElement('img');
+    const sH2Name = document.createElement('h2');
+    sH2Name.className = 'black';
+    const sJobTitle = document.createElement('p');
+    sJobTitle.className = 'job-title orange';
+    const sWord = document.createElement('p');
+    sWord.className = 'black s-word';
+    imgSpeaker.src = speakerDataArr[b].sImg;
+    sH2Name.textContent = speakerDataArr[b].sName;
+    sJobTitle.textContent = speakerDataArr[b].jobTitle;
+    sWord.textContent = speakerDataArr[b].sWords;
+    s_img.appendChild(imgSpeaker);
+    speaker.appendChild(s_img);
+    speakerList.appendChild(speaker);
+    s_details.appendChild(sH2Name);
+    s_details.appendChild(sJobTitle);
+    s_details.appendChild(s_bar);
+    s_details.appendChild(sWord);
+    speaker.appendChild(s_details);
+    speakerList.appendChild(speaker);
+  }
+  return speakerList;
+}
+
+speakerDataAdd();
