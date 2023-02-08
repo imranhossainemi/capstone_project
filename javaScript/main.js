e6ce6a566da83a951c24ac08ea9e7a4902979812
imranhossainemi <blur-imran@hotmail.com>
2023-02-08 11:44:15 +0600

    Update main.js

--- javaScript/main.js
+++ javaScript/main.js
@@ -1,2 +1,6 @@
 const menuButton = document.querySelector('.nav-btn');
 const menu = document.querySelector('.main-menu');
+menuButton.addEventListener('click', () => {
+  menuButton.classList.toggle('active');
+  menu.classList.toggle('active');
+});
